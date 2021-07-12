e00b662170cfa3c4a5f67037402169b1c3cdcd75
stevePC <sgantney@ucsc.edu>
2021-07-12 15:50:05 -0700

    drag and drop works

--- main.py
+++ main.py
@@ -7,24 +7,30 @@ pygame.display.set_caption("Dumb")
 win = pygame.display.set_mode((width, height))
 
 
-def draw_window(screen, player):
+def draw_window(screen, players):
     screen.fill((255, 255, 255))
-    player.draw(screen)
+    for player in players:
+        player.draw(screen)
     pygame.display.update()
 
 
 def main():
     run = True
     clock = pygame.time.Clock()
-    player = Player(50, 50, 100, 100, (255, 0, 0))
+    player1 = Player(50, 50, 100, 100, (255, 0, 0))
+    player2 = Player(100, 100, 100, 100, (255, 0, 0))
+    players = [player1, player2]
     while run:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        player.move()
-        draw_window(win, player)
+        for player in players:
+            if player.move():
+                players.insert(0, players.pop(players.index(player)))
+                break
+        draw_window(win, players)
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -10,17 +10,25 @@ class Player:
         self.color = color
         self.rect = (x, y, width, height)
         self.vel = .1
+        self.click = False
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, self.rect)
+
+    def clicked(self, mouse_x, mouse_y, left):
+        if left:
+            if self.x + self.width > mouse_x > self.x and self.y + self.height > mouse_y > self.y:
+                self.click = True
+        else:
+            self.click = False
 
     def move(self):
         mouse_x, mouse_y = pygame.mouse.get_pos()
         center_x = mouse_x - self.width/2
         center_y = mouse_y - self.height/2
         left, middle, right = pygame.mouse.get_pressed(num_buttons=3)
-        if left:
-            self.color = (255, 0, 0)
+        self.clicked(mouse_x, mouse_y, left)
+        if left and self.click:
             x_dist = abs(center_x - self.x)
             y_dist = abs(center_y - self.y)
             if center_x < self.x:
@@ -32,6 +40,10 @@ class Player:
             if center_y > self.y:
                 self.y += self.vel * y_dist
         self.update()
+        if self.click:
+            return True
+        else:
+            return False
 
     def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
