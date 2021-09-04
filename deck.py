a3885e82606a03630e277e26faff8dba83418936
sgantney <72237856+sgantney@users.noreply.github.com>
2021-09-04 15:33:34 -0700

    manual push

--- deck.py
+++ deck.py
@@ -1,31 +1,25 @@
-import os
-import pygame
-import random
-
-from datetime import datetime
+import pygame, sys, os
 from card import Card
 
 
-class Deck:
-    def __int__(self, card_x, card_y, card_width, card_height, cards_file, jokers):
-        self.card_x = card_x
-        self.card_y = card_y
-        self.card_width = card_width
-        self.card_height = card_height
-        self.cards_file = cards_file
-        self.cards_list = os.listdir(cards_file)
-        self.jokers = jokers
-        self.cards_left = 54 if jokers else 52
+class Deck(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, image_folder, back_path):
+        super().__init__()
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.image_folder = image_folder
+        self.cards_list = os.listdir(image_folder)
+        self.back = pygame.image.load(os.path.join(image_folder, back_path))
+        self.image = self.back
+        self.rect = self.image.get_rect()
+        self.rect.center = (pos_x, pos_y)
+        self.deck = pygame.sprite.Group()
 
-    def generate_card(self):
-        random.seed(datetime.now())  # creates random seed
-        rand = random.randint(0, 10000) % self.cards_left  # grabs random card index
-        card = Card(self.card_x, self.card_y, self.card_width, self.card_height, False, self.cards_list[rand])
-        card.image = pygame.image.load(os.path.join(self.cards_file, card.card_name))
-        card.image.convert()
-        self.cards_list.pop(rand)
-        return card
+    def clicked(self):
+        return pygame.mouse.get_pressed(3)[0] and self.rect.collidepoint(pygame.mouse.get_pos())
 
-
-
-    #def generate_deck
+    def add_card(self):
+        card_name = self.cards_list.pop(0)
+        card = Card(self.pos_x, self.pos_y, self.image_folder, card_name, self.back, True)
+        card.held = True
+        self.deck.add(card)
